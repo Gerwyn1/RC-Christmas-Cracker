7c5ad5c2987ea29d23acb707b6c6f1425bd07e74
Gerwyn <gerwynsim@hotmail.com>
2023-12-12 16:18:36 +0800

    piece up stage 1 image

--- src/components/PickCracker.jsx
+++ src/components/PickCracker.jsx
@@ -4,20 +4,7 @@ import { useEffect, useRef } from "react";
 
 // eslint-disable-next-line react/prop-types
 const PickCracker = ({ setStage, setColor }) => {
-  const crackerPicked = useRef()
-  // const beforeStyles = {
-  //   content: '""',
-  //   background: 'url("/newImages/spear-sword.png")',
-  //   backgroundRepeat: "no-repeat",
-  //   backgroundSize: "contain",
-  //   position: "absolute",
-  //   bottom: "-10%",
-  //   right: "-20%",
-  // };
-
-  // useEffect(() => {
-
-  // }, [])
+  const crackerPicked = useRef();
 
   const crackerAnimation = {
     animation: "shake 0.82s cubic-bezier(.36,.07,.19,.97) both",
@@ -25,6 +12,12 @@ const PickCracker = ({ setStage, setColor }) => {
     backfaceVisibility: "hidden",
     perspective: "1000px",
   };
+
+
+  // onClick={() => {
+  //   setStage("cracker picked");
+  //   setColor("g");
+  // }}
 
   return (
     <div
@@ -35,98 +28,146 @@ const PickCracker = ({ setStage, setColor }) => {
       }}
     >
       <img
-        src={"/Images/WebP/cover_bg.webp"}
+        src={"/public/RC_stage1.png"}
         style={{
           position: "relative",
           height: "100%",
           width: "100%",
         }}
       />
-      {/* PRESENT */}
-      <img
-        className="cropped-bag"
-        style={{
-          position: "absolute",
-          bottom: "0%",
-          right: "6%",
-          height: "30%",
-          zIndex: 9999,
-        }}
-        src="/public/Images/Webp/popper_cover.webp"
-      />
-
-      {/* GREEN CRACKER */}
-      <img
-        style={{
-          position: "absolute",
-          maxWidth: "16.5%",
-          right: "26%",
-          bottom: "15%",
-          zIndex: 9998,
-          transform: "rotate(-10deg)",
-          cursor: "pointer",
-          //
-        }}
-        src="/Images/WebP/pop_g.webp"
-        onClick={() => {
-          setStage("cracker picked");
-          setColor("g");
-        }}
-      />
-      {/* RED CRACKER */}
-      <img
-        style={{
-          position: "absolute",
-          maxWidth: "16.5%",
-          bottom: "10%",
-          right: "37.5%",
-          zIndex: 9995,
-          transform: "rotate(-18.5deg)",
-          cursor: "pointer",
-        }}
-        src="/Images/WebP/pop_r.webp"
-        onClick={() => {
-          setStage("cracker picked");
-          setColor("r");
-        }}
-      />
-      {/* PURPLE CRACKER */}
-      <img
-        style={{
-          position: "absolute",
-          maxWidth: "16.5%",
-          bottom: "20%",
-          right: "60%",
-          zIndex: 9999,
-          transform: "rotate(-30.5deg)",
-          cursor: "pointer",
-        }}
-        src="/Images/WebP/pop_p.webp"
-        onClick={() => {
-          setStage("cracker picked");
-          setColor("p");
-        }}
-        useMap="#image-map"
-      />
-      {/* WHITE CRACKER */}
-      <img
-        style={{
-          position: "absolute",
-          maxWidth: "16.5%",
-          bottom: "5%",
-          right: "55%",
-          zIndex: 9998,
-          transform: "rotate(-47.5deg)",
-          cursor: "pointer",
-        }}
-        src="/Images/WebP/pop_w.webp"
-        onClick={() => {
-          setStage("cracker picked");
-          setColor("w");
-        }}
-      />
     </div>
+
+    // <div
+    //   style={{
+    //     position: "relative",
+    //     height: "100vh",
+    //     overflow: "hidden",
+    //   }}
+    // >
+    //   <img
+    //     src={"/Images/WebP/cover_bg.webp"}
+    //     style={{
+    //       position:'relative',
+    //       height: "100%",
+    //       width: "100%",
+    //     }}
+    //   />
+
+    //     {/* PRESENT */}
+    //     <img
+    //       className="cropped-present"
+    //       style={{
+    //         position: "absolute",
+    //         bottom: "0%",
+    //         right: "6%",
+    //         width: "58.5%",
+    //         zIndex: 9999,
+    //       }}
+    //       src="/Revised RC images/popper_cover.webp"
+    //     />
+
+    //     {/* PRESENT BACK*/}
+    //     <img
+    //       // className="cropped-bag"
+    //       style={{
+    //         position: "absolute",
+    //         bottom: "18.25%",
+    //         right: "20%",
+    //         width: "45.25%",
+    //         zIndex: 5,
+    //       }}
+    //       src="/Revised RC images/box_back.webp"
+    //     />
+
+    // </div>
   );
 };
 
 export default PickCracker;
+
+{
+  /* GREEN CRACKER */
+}
+{
+  /* <img
+  style={{
+    position: "absolute",
+    maxWidth: "16.5%",
+    right: "25%",
+    bottom: "18%",
+    zIndex: 9998,
+    transform: "rotate(-10deg)",
+    cursor: "pointer",
+    //
+  }}
+  src="/Images/WebP/pop_g.webp"
+  onClick={() => {
+    setStage("cracker picked");
+    setColor("g");
+  }}
+/> */
+}
+{
+  /* RED CRACKER */
+}
+{
+  /* <img
+  style={{
+    position: "absolute",
+    maxWidth: "16.5%",
+    bottom: "10%",
+    right: "36%",
+    zIndex: 9995,
+    transform: "rotate(-18.5deg)",
+    cursor: "pointer",
+  }}
+  src="/Images/WebP/pop_r.webp"
+  onClick={() => {
+    setStage("cracker picked");
+    setColor("r");
+  }}
+/> */
+}
+{
+  /* PURPLE CRACKER */
+}
+{
+  /* <img
+  style={{
+    position: "absolute",
+    maxWidth: "16.5%",
+    bottom: "20%",
+    right: "59%",
+    zIndex: 9999,
+    transform: "rotate(-30.5deg)",
+    cursor: "pointer",
+  }}
+  src="/Images/WebP/pop_p.webp"
+  onClick={() => {
+    setStage("cracker picked");
+    setColor("p");
+  }}
+  useMap="#image-map"
+/> */
+}
+{
+  /* WHITE CRACKER */
+}
+{
+  /* <img
+  style={{
+    position: "absolute",
+    maxWidth: "16.5%",
+    bottom: "5%",
+    right: "54%",
+    zIndex: 9998,
+    transform: "rotate(-47.5deg)",
+    cursor: "pointer",
+  }}
+  src="/Images/WebP/pop_w.webp"
+  onClick={() => {
+    setStage("cracker picked");
+    setColor("w");
+  }}
+/> */
+}
